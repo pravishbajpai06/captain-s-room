a34eca335b9ae43ea787f3a38c42f217b3deac96
Pravish Bajpai <55047219+pravishbajpai06@users.noreply.github.com>
2020-08-28 23:45:14 +0530

    Update captainroom.py

--- captainroom.py
+++ captainroom.py
@@ -1,1 +1,14 @@
+n=int(input())
+rooms=set(map(int,input().split()))
+a=set()
+b=set()
+for room in rooms:
+  if room not in a:
+    a.add(room)
+    b.add(room)
+  else:
+    b.discard(room)
+b=list(b)
+print(b[0])
+    
 
